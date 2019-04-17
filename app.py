7217935f530e81da922b22c4cae63b0fba124d77
aaronchu415 <aaronchu415@gmail.com>
2019-04-17 10:46:54 -0700

    set up boiler plate code

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, request, render_template, redirect, flash, session, jsonify
 from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = "SECRETTT"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.secret_key = "ADEFADEAA"
+
+debug = DebugToolbarExtension(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
--- models.py
+++ models.py
@@ -1,1 +1,29 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """User Model"""
+
+    __tablename__ = "Users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String(50),
+                           nullable=False)
+    last_name = db.Column(db.String(50),
+                          nullable=False)
+    image_url = db.Column(db.String(200))
+
+    def __repr__(self):
+        return f'<{self.id},{self.first_name},{self.last_name}>'
